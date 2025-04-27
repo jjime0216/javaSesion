ceeb1d107dcbf99a4c6a6a1d2c3099a52d43fcb9
jjime0216 <jeuli_98@hotmail.com>
2025-04-26 21:40:40 -0500

    Proyecto prueba de Java maven con base de datos simple para inicio de sesion base de datos.

---
+++ src/main/java/sena/ejercicioproyecto1/Ejercicioproyecto1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sena.ejercicioproyecto1;
+
+import sena.ejercicioproyecto1.view.LoginPage;
+
+
+/**
+ *
+ * @author Julian
+ */
+public class Ejercicioproyecto1 {
+
+    public static void main(String[] args) {
+        
+        LoginPage princ = new LoginPage();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+    }
+}
